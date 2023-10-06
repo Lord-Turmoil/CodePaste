cb425117074ca593cc660533867e06e8a1a36d76
Lord Turmoil <tony-turmoil@outlook.com>
2023-10-06 19:01:01 +0800

    fix: theme background color

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 // normalize plugin
-Prism.plugins.NormalizeWhitespace.setDefaults({
-    'remove-trailing': true,
-    'remove-indent': true,
-    'left-trim': true,
-    'right-trim': true,
-    // 'break-lines': 80,
-    // 'indent': 4,
-    // 'remove-initial-line-feed': true,
-    'tabs-to-spaces': 4
-    // 'spaces-to-tabs': 4
-});
+// Prism.plugins.NormalizeWhitespace.setDefaults({
+//     'remove-trailing': true,
+//     'remove-indent': true,
+//     'left-trim': true,
+//     'right-trim': true,
+//     // 'break-lines': 80,
+//     // 'indent': 4,
+//     // 'remove-initial-line-feed': true,
+//     'tabs-to-spaces': 4
+//     // 'spaces-to-tabs': 4
+// });
 
 alertify.set('notifier', 'delay', 1.5);
 
