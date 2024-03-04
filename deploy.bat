99c3c9d679e11e9d31007004b47f510dd34a165b
Lord Turmoil <tony-turmoil@outlook.com>
2024-03-04 13:56:55 +0800

    feat: add deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+echo off
+
+mkdir temp
+
+@REM copy css, js, res folder into temp
+xcopy /s /e /y /q css temp\css\
+xcopy /s /e /y /q js temp\js\
+xcopy /s /e /y /q res temp\res\
+@REM copy favicon.png, index.html into temp
+xcopy /q favicon.png temp
+xcopy /q index.html temp
+@REM copy publish
+call PseudoFTP.Client transfer -s temp -i CodePaste --overwrite
+rmdir /s /q temp
