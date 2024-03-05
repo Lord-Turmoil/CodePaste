18ad7d90b7a3eb647e5a1e23643c413bb8ab7583
Lord Turmoil <tony-turmoil@outlook.com>
2024-03-05 21:11:26 +0800

    feat: add .ftpignore

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,3 @@
-@echo off
-
-mkdir temp
-
-@REM copy css, js, res folder into temp
-xcopy /s /e /y /q css temp\css\
-xcopy /s /e /y /q js temp\js\
-xcopy /s /e /y /q res temp\res\
-@REM copy favicon.png, index.html into temp
-xcopy /q favicon.png temp
-xcopy /q index.html temp
-@REM copy publish
-call PseudoFTP.Client transfer -s temp -i CodePaste --overwrite
-rmdir /s /q temp
+@ECHO OFF
+call PseudoFTP.Client transfer -s . -l CodePaste --overwrite
+PAUSE
